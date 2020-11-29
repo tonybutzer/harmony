838261ead7b4030481eb6e459a4123d47939e202
tonybutzer <tonybutzer@gmail.com>
2020-11-29 20:33:24 +0000

    automatic git update from Makefile

--- devops/tfinfra/chs-tljh/ship.tf
+++ devops/tfinfra/chs-tljh/ship.tf
@@ -17,7 +17,7 @@ resource "aws_instance" "master" {
   iam_instance_profile                    ="${var.iam_role}"
 
   security_groups = ["${var.security_group_ssh}", "${var.security_group_ping}", "${var.security_group_web}"]
-  root_block_device {volume_size = 40}
+  root_block_device {volume_size = 240}
 
   user_data                   = "${file("files/os_boot.sh")}"
 
